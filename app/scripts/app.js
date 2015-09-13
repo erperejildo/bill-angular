135471d8c1c2751fbac28fff8899d8482f8eeabf
erperejido <danixiwaka@gmail.com>
2015-09-13 15:51:33 +0200

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name billAngularJsApp
+ * @description
+ * # billAngularJsApp
+ *
+ * Main module of the application.
+ */
+angular
+	.module('billAngularJsApp', [
+		'ngAnimate'
+	]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name billAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the billAngularJsApp
+ */
+angular.module('billAngularJsApp')
+	.controller('MainCtrl', function($scope, $http) {
+
+		$scope.bill = null;
+
+		$http.get('https://still-scrubland-9880.herokuapp.com/bill.json').
+		then(function(response) {
+			$scope.error = false;
+			$scope.bill  = response.data;
+			$scope.total = ($scope.bill.package.total + $scope.bill.callCharges.total + $scope.bill.skyStore.total).toFixed(2);
+		}, function(response) {
+			$scope.error = true;
+		});
+
+		// order data
+		$scope.orderSubcriptions = function(predicate) {
+			$scope.reverseSub = ($scope.subcription === predicate) ? !$scope.reverseSub : false;
+			$scope.subcription = predicate;
+		};
+
+		$scope.orderCalls = function(predicate) {
+			$scope.reverseCalls = ($scope.call === predicate) ? !$scope.reverseCalls : false;
+			$scope.call = predicate;
+		};
+
+		$scope.orderRentals = function(predicate) {
+			$scope.reverseRentals = ($scope.rental === predicate) ? !$scope.reverseRentals : false;
+			$scope.rental = predicate;
+		};
+
+		$scope.orderBoughts = function(predicate) {
+			$scope.reverseBought = ($scope.bought === predicate) ? !$scope.reverseBought : false;
+			$scope.bought = predicate;
+		};
+
+	});
